62dbf7d8eede00673ea463d4bfa090bb89e8e62f
Felipe Santos <fecsantos@hotmail.com>
2024-08-31 18:02:04 +0100

    Initial commit

---
+++ test/models/user_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  first_name             :string
+#  last_name              :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
